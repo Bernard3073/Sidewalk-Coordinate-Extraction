5ff1c0f41378fa3d9c6e4fb804b38da7f411e8d4
Bernard3073 <bert41003@gmail.com>
2021-09-14 16:21:26 -0400

    finish calculating local orientation and edge strength

--- main.cpp
+++ main.cpp
@@ -2,47 +2,42 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <string>
+#include <math.h>
 
-using namespace cv;
 
-void image_grad(Mat img, Mat &img_x, Mat &img_y){
+
+void sanityCheck(cv::Mat checkMatrix);
+
+void image_grad(cv::Mat img, cv::Mat &img_x, cv::Mat &img_y){
 
   float sobelx_data[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
   float sobely_data[9] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
-  Mat sobelx = Mat(3, 3, CV_32F, sobelx_data);
-  Mat sobely = Mat(3, 3, CV_32F, sobely_data);
-  // Mat img_x, img_y;
+  cv::Mat sobelx = cv::Mat(3, 3, CV_32F, sobelx_data);
+  cv::Mat sobely = cv::Mat(3, 3, CV_32F, sobely_data);
+
   filter2D(img, img_x, -1, sobelx);
   filter2D(img, img_y, -1, sobely);
 
-  // imshow("r", img_x);
-  // return img_x, img_y;
+  // cv::imshow("r", img_x);
+  // cv::waitKey(0);
+  // cv::destroyAllWindows();
 }
 
-void color_tensor(Mat &res, Mat Blue_x, Mat Blue_y, Mat Green_x, Mat Green_y, Mat Red_x, Mat Red_y, bool x_dir, bool y_dir){
-  Mat output;
+void color_tensor(cv::Mat &res, cv::Mat Blue_x, cv::Mat Blue_y, cv::Mat Green_x, cv::Mat Green_y, cv::Mat Red_x, cv::Mat Red_y, bool x_dir, bool y_dir){
   if(x_dir && !y_dir){
-    // add(Blue_x.mul(Blue_x), Green_x.mul(Green_x), output);
-    // std::cout << "output row: 0~2 = "<< std::endl << " "  << output.rowRange(0, 2) << std::endl << std::endl;
-    // add(output, Red_x.mul(Red_x), res);
-    // std::cout << "output row: 0~2 = "<< std::endl << " "  << output.rowRange(0, 2) << std::endl << std::endl;
     res = Blue_x.mul(Blue_x) + Green_x.mul(Green_x) + Red_x.mul(Red_x);
   }
   else if(!x_dir && y_dir){
-    // add(Blue_y.mul(Blue_y), Green_y.mul(Green_y), output);
-    // add(output, Red_y.mul(Red_y), res);
     res = Blue_y.mul(Blue_y) + Green_y.mul(Green_y) + Red_y.mul(Red_y);
   }
   else if(x_dir && y_dir){
-    // add(Blue_x.mul(Blue_y), Green_x.mul(Green_y), output);
-    // add(output, Red_x.mul(Red_y), res);
     res = Blue_x.mul(Blue_y) + Green_x.mul(Green_y) + Red_x.mul(Red_y);
   }
   // res = output;
 }
 
 // For Debug
-// https://stackoverflow.com/questions/10167534/how-to-find-out-what-type-of-a-mat-object-is-with-mattype-in-opencv
+// https://stackoverflow.com/questions/10167534/how-to-find-out-what-type-of-a-cv::mat-object-is-with-cv::mattype-in-opencv
 std::string type2str(int type) {
   std::string r;
   uchar depth = type & CV_MAT_DEPTH_MASK;
@@ -65,13 +60,14 @@ std::string type2str(int type) {
   return r;
 }
 
-void sanityCheck(Mat checkMatrix)
+void sanityCheck(cv::Mat checkMatrix)
 {
   std::cout << std::endl << "Matrix holds:" <<std::endl;
-  for (int r = 0; r < 5; r++)
+  for (int r = 0; r < checkMatrix.rows; r++)
   {
-    for (int c = 0; c < 5; c++)
+    for (int c = 0; c < checkMatrix.cols; c++)
     {
+      // unsigned char is an unsigned byte value(0-255)
       std::cout << checkMatrix.at<double>(r, c) << " | ";
     }
     std::cout << std::endl;
@@ -80,43 +76,53 @@ void sanityCheck(Mat checkMatrix)
 
 int main( int argc, char** argv ) {
   
-  Mat image;
-  image = imread("0.png" ,IMREAD_COLOR);
+  cv::Mat image;
+  image = cv::imread("0.png");
   
   if(! image.data ) {
       std::cout <<  "Image not found or unable to open" << std::endl ;
       return -1;
   }
   
-  Mat rgbchannel[3];
-  split(image, rgbchannel);
-  Mat Red = rgbchannel[0];
-  Mat Green = rgbchannel[1];
-  Mat Blue = rgbchannel[2];
+  // cv::Mat three_channels[3];
+  // cv::split(image, three_channels);
+  cv::Mat three_channels[3]; 
+  cv::split(image, three_channels);
+  cv::Mat Blue = cv::Mat::zeros(cv::Size(image.cols+1, image.rows+1), CV_32F);
+  cv::Mat Green = cv::Mat::zeros(cv::Size(image.cols+1, image.rows+1), CV_32F);
+  cv::Mat Red = cv::Mat::zeros(cv::Size(image.cols+1, image.rows+1), CV_32F);
+  for (int r = 0; r < image.rows; r++)
+  {
+    for (int c = 0; c < image.cols; c++)
+    {
+      // Blue.at<uchar>(r, c) = image.at<cv::Vec3b>(r, c)[0];
+      // Green.at<uchar>(r, c) = image.at<cv::Vec3b>(r, c)[1];
+      // Red.at<uchar>(r, c) = image.at<cv::Vec3b>(r, c)[2];
+      Blue.at<float>(r, c) = three_channels[0].at<uchar>(r, c);
+      Green.at<float>(r, c) = three_channels[1].at<uchar>(r, c);
+      Red.at<float>(r, c) = three_channels[2].at<uchar>(r, c);
+    }
+    // std::cout << std::endl;
+  }
 
-  // cv::imshow("red", Red);
-  // cv::imshow("green", Green);
-  // cv::imshow("blue", Blue);
-  // waitKey(0);
-  // destroyAllWindows();
-  Mat Blue_x, Blue_y, Green_x, Green_y, Red_x, Red_y;
-  Blue_x.convertTo(Blue_x, CV_8U);
-  sanityCheck(image);
+  cv::Mat Blue_x, Blue_y, Green_x, Green_y, Red_x, Red_y;
   image_grad(Blue, Blue_x, Blue_y);
   image_grad(Green, Green_x, Green_y);
   image_grad(Red, Red_x, Red_y);
 
+  // sanityCheck(Blue);
+
   // imshow("B", Blue_x);
-  Mat G_xx, G_yy, G_xy;
+  // cv::Mat G_xx = cv::Mat::zeros(cv::Size(Blue_x.rows, Blue_x.cols), CV_32F);
+  // cv::Mat G_yy = cv::Mat::zeros(cv::Size(Blue_x.rows, Blue_x.cols), CV_32F);
+  // cv::Mat G_xy = cv::Mat::zeros(cv::Size(Blue_x.rows, Blue_x.cols), CV_32F);
+  cv::Mat G_xx, G_yy, G_xy;
   color_tensor(G_xx, Blue_x, Blue_y, Green_x, Green_y, Red_x, Red_y, true, false);
   color_tensor(G_yy, Blue_x, Blue_y, Green_x, Green_y, Red_x, Red_y, false, true);
   color_tensor(G_xy, Blue_x, Blue_y, Green_x, Green_y, Red_x, Red_y, true, true);
 
   // std::cout << "G_xx row: 0~2 = "<< std::endl << " "  << G_xx.rowRange(0, 2) << std::endl << std::endl;
   // std::cout << "G_yy row: 0~2 = "<< std::endl << " "  << G_yy.rowRange(0, 2) << std::endl << std::endl;
-  G_xx.convertTo(G_xx, CV_32F);
-  G_yy.convertTo(G_yy, CV_32F);
-  G_xy.convertTo(G_xy, CV_32F);
 
   // For Debug
   // std::string ty =  type2str( G_xy.type() );
@@ -124,25 +130,62 @@ int main( int argc, char** argv ) {
   
   // local_orientation = 0.5*np.arctan2(2*G_xy, G_xx - G_yy) + (np.pi / 2)
   // edge_strength = 0.5*(G_xx + G_yy + np.sqrt(np.square(G_xx - G_yy) + 4*np.square(G_xy)))
-  Mat tensor_angle, local_orientation, edge_strength; 
-  Mat xx_sub_yy;
+  cv::Mat tensor_angle; 
+  cv::Mat xx_sub_yy;
   // subtract(G_xx, G_yy, xx_sub_yy);
+  xx_sub_yy = G_xx - G_yy;
   // phase(xx_sub_yy, 2*G_xy, tensor_angle);
   // add(0.5*tensor_angle, CV_PI / 2, local_orientation);
   // phase(xx_sub_yy, 2*G_xy, local_orientation);
-  sanityCheck(G_xx);
+  // sanityCheck(G_xx);
+  // sanityCheck(G_yy);
+  // sanityCheck(G_xy);
+
+  G_xx.convertTo(G_xx, CV_32F);
+  G_yy.convertTo(G_yy, CV_32F);
+  G_xy.convertTo(G_xy, CV_32F);
+
+  cv::Mat local_orientation;
   phase(G_xx - G_yy, 2*G_xy, local_orientation);
-  local_orientation = 0.5*local_orientation + (CV_PI / 2);
-  Mat xx_yy_square, xy_square;
+  
+  // cv::Mat local_orientation = cv::Mat::zeros(cv::Size(G_xy.rows, G_xy.cols), CV_32F);
+  // for (int r = 0; r < G_xy.rows; r++)
+  // {
+  //   for (int c = 0; c < G_xy.cols; c++)
+  //   {
+  //     local_orientation.at<float>(r, c) =  atan2(2*G_xy.at<float>(r, c), xx_sub_yy.at<float>(r, c)) + (CV_PI/2);
+  //     // std::cout << local_orientation.at<double>(r, c) << " | ";
+  //   }
+  //   // std::cout << std::endl;
+  // }
+
+  // local_orientation = 0.5*local_orientation + (CV_PI / 2);
+  cv::Mat xx_yy_square, xy_square;
   pow(G_xx - G_yy, 2.0, xx_yy_square);
   pow(G_xy, 2.0, xy_square);
-  Mat sqrt_x_y, add_x_y, add_xx_yy;
+  cv::Mat sqrt_x_y, add_x_y, add_xx_yy;
   add(xx_yy_square, 4*xy_square, add_x_y);
   sqrt(add_x_y, sqrt_x_y);
-  // add(G_xx, G_yy, add_xx_yy);
-  // add(add_xx_yy, sqrt_x_y, edge_strength);
+  // cv::Mat edge_strength = cv::Mat::zeros(cv::Size(G_xy.rows, G_xy.cols), CV_32F);
+  cv::Mat edge_strength;
+  // for (int r = 0; r < G_xy.rows; r++)
+  // {
+  //   for (int c = 0; c < G_xy.cols; c++)
+  //   {
+  //     edge_strength.at<double>(r, c) = 0.5*(G_xx.at<double>(r, c) + G_yy.at<double>(r, c) + sqrt(pow(xx_sub_yy.at<double>(r, c), 2.0) + 4*pow(G_xy.at<double>(r, c), 2.0)));
+  //     // std::cout << edge_strength.at<double>(r, c) << " | ";
+  //   }
+  //   // std::cout << std::endl;
+  // }
   edge_strength = 0.5*(G_xx + G_yy + sqrt_x_y);
-
+  // for(int r=0; r<5; r++){
+  //   for(int c=0; c<5; c++){
+  //     // std::cout << edge_strength.at<double>(r, c) << "|";
+  //     std::cout << xx_sub_yy.at<uchar>(r, c) << "|";
+      
+  //   }
+  //   std::cout << std::endl;
+  // }
   // edge_strength = np.uint(255*abs(edge_strength)) / np.max(abs(edge_strength))
   // img_binary = np.zeros_like(edge_strength)
   // # setting threshold
@@ -150,12 +193,12 @@ int main( int argc, char** argv ) {
   //         (edge_strength <= 255)] = 1
   double minVal, maxVal;
   minMaxLoc(edge_strength, &minVal, &maxVal);
-  Mat res;
+  cv::Mat res;
   // normalize(edge_strength, res, minVal, maxVal, NORM_MINMAX);
   edge_strength = 255*edge_strength / maxVal;
   imshow("e", edge_strength);
 
-  waitKey(0);
-  destroyAllWindows();
+  cv::waitKey(0);
+  cv::destroyAllWindows();
   return 0;
 }
